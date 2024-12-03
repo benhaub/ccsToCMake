e923dacc6da9b7f4732e32d7b76a63bc5e05e862
Ben Haubrich <bjh885@mail.usask.ca>
2024-12-03 18:48:49 -0500

    Get rid of any mention of any hardcoded mention of a specific target in the toolchain.cmake and move it all over to ccsProject.cmake. Replace directory and file copying with symbolic links. Add a missing required library for driverlib.

---
+++ ccsProject.cmake
@@ -0,0 +1,38 @@
+#You can find the correct port just by opening up an example project in code composer studio
+#and checking where the project is grabbing the freertos portable files from
+set(PORT GCC/ARM_CM3)
+
+#TODO: Add in symlink for windows.
+if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux" OR ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin")
+  #Set this to the install location of the SDK. It's best to install and compile the SDK using code composer studio first.
+  set(SDK $ENV{HOME}/ti/simplelink_cc32xx_sdk_7_10_00_13)
+  #Name of the link.
+  set(SDK_LINK simplelink_cc32xx_sdk_7_10_00_13)
+  #Set this to the location of the Code Composer Studio Project you want run. This CMake
+  #file will automatically copy over any required files.
+  set(CCS_PROJECT $ENV{HOME}/workspace_v12/buttonled_CC3220SF_LAUNCHXL_freertos_gcc)
+  set(CCS_PROJECT_LINK buttonled_CC3220SF_LAUNCHXL_freertos_gcc)
+  #Set this to the sysconfig version you want. For default projects there is usually a Debug and MCU+Image version.
+  set(SYSCONFIG_TYPE Debug)
+
+  #execute process runs prior to build system generation. add_custom_command runs during so it can't be used.
+  #Creates a symlink to the SDK and reference CCS project.
+  execute_process(
+  COMMAND
+    rm -f ${SDK_LINK}
+  COMMAND
+    rm -f ${CCS_PROJECT_LINK}
+  COMMAND
+    ln -T ${SDK} -s ${SDK_LINK}
+  COMMAND
+    ln -T ${CCS_PROJECT} -s ${CCS_PROJECT_LINK}
+  WORKING_DIRECTORY
+    ${CMAKE_CURRENT_LIST_DIR}
+  COMMAND_ECHO
+    STDOUT
+  COMMAND_ERROR_IS_FATAL
+      ANY
+  )
+else()
+  message(FATAL_ERROR "Only Linux or MacOS is supported")
+endif()
--- toolchain.cmake
+++ toolchain.cmake
@@ -1,8 +1,7 @@
+include(${CMAKE_CURRENT_LIST_DIR}/ccsProject.cmake)
+
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
-
-#You can use this variable in other CMake files to check if you are compiler for a cc32xx
-set(CC32xx 1)
 
 set(tools $ENV{HOME}/ti/ccs1281/ccs/tools/compiler/gcc-arm-none-eabi-9-2019-q4-major)
 set(CMAKE_C_COMPILER ${tools}/bin/arm-none-eabi-gcc)
@@ -13,11 +12,10 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_C_FLAGS "-std=c99 ${CMAKE_C_FLAGS}")
 set(CMAKE_CXX_FLAGS "-std=c++2a ${CMAKE_CXX_FLAGS}")
 
+#CCS_PROJECT_SYMLINK is set in ccsProject.cmake
+file(GLOB linkerScript ${CMAKE_CURRENT_LIST_DIR}/${CCS_PROJECT_LINK}/*.lds)
+
 add_compile_options(
-  -ffunction-sections
-  -fdata-sections
-  -fno-exceptions
-  -Wall
   -march=armv7e-m
   -mthumb
   -mfloat-abi=soft
@@ -26,9 +24,10 @@ add_compile_options(
 add_link_options(
   -nostartfiles
   -static
-  -Wl,-T${CMAKE_CURRENT_LIST_DIR}/cc32xxsf_freertos.lds
+  -specs=nano.specs
+  -Wl,-T${linkerScript}
   -Wl,--gc-sections
-  -Wl,-Map,autoFeeder.map
+  -Wl,-Map,cc32xx.map
 )
 
 link_libraries(
@@ -39,3 +38,4 @@ link_libraries(
   nosys
 )
 include_directories(${tools}/arm-none-eabi/lib/thumb/v7e-m/nofp/)
+include_directories(${tools}/arm-none-eabi/include/newlib-nano)
