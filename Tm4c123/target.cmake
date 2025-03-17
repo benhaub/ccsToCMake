246526c10c1e1af6b5e2125ca4bd51506cdbfcd8
Ben Haubrich <bjh885@mail.usask.ca>
2025-03-17 18:24:04 -0400

    Add support for Tm4c123 projects.

---
+++ Tm4c123/target.cmake
@@ -0,0 +1,45 @@
+target_sources(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PRIVATE FILE_SET headers TYPE HEADERS BASE_DIRS ${CMAKE_CURRENT_LIST_DIR} FILES
+  ${startupHeaders}
+)
+#Add the sysconfig generated source files. These are set in ccsProject.cmake
+target_sources(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PRIVATE
+  ${startupSources}
+  ${thirdPartySourceFiles}
+)
+
+target_include_directories(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PRIVATE
+  ${CMAKE_CURRENT_LIST_DIR}/${SDK_LINK}/third_party/FreeRTOS/Source/include
+  ${CMAKE_CURRENT_LIST_DIR}/${SDK_LINK}/third_party/FreeRTOS/Source/portable
+  ${CMAKE_CURRENT_LIST_DIR}/${SDK_LINK}/third_party/FreeRTOS/Source/portable/${PORT}
+)
+
+get_target_property(Includes ${PROJECT_NAME}${EXECUTABLE_SUFFIX} INCLUDE_DIRECTORIES)
+message(STATUS "Includes: ${Includes}")
+
+target_include_directories(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/${SDK_LINK}
+)
+
+target_include_directories(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/${CCS_PROJECT_LINK}
+)
+
+target_link_directories(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/${SDK_LINK}/driverlib/gcc
+)
+#The order in which you link libraries matters!
+target_link_libraries(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PUBLIC
+    driver
+)
+
+target_compile_definitions(${PROJECT_NAME}${EXECUTABLE_SUFFIX}
+PUBLIC
+    PART_TM4C123GH6PM
+)
---
+++ Tm4c123/toolchain.cmake
@@ -0,0 +1,57 @@
+include(${CMAKE_CURRENT_LIST_DIR}/ccsProject.cmake)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(tools $ENV{HOME}/ti/ccs1281/ccs/tools/compiler/gcc-arm-none-eabi-9-2019-q4-major)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-none-eabi-g++)
+#See https://cmake.org/cmake/help/latest/variable/CMAKE_TRY_COMPILE_TARGET_TYPE.html
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_C_FLAGS "-std=c2x ${CMAKE_C_FLAGS}")
+set(CMAKE_CXX_FLAGS "-std=c++2a ${CMAKE_CXX_FLAGS}")
+
+#CCS_PROJECT_LINK is set in ccsProject.cmake
+file(GLOB linkerScript ${CMAKE_CURRENT_LIST_DIR}/${CCS_PROJECT_LINK}/*.lds)
+
+#The driver lib is built with a hard fp and so anything that links it must also use a hard fp
+#cxa_atexit information: https://itanium-cxx-abi.github.io/cxx-abi/abi.html#dso-dtor-motivation
+#On an embedded system, there is no need to clean up after the program exits unless you were running
+#a multi-process operating system like Linux. Other TI Cortex M4 devices like Cc32xx define _dso_handle
+#in startup_cc32xx_gcc.c but don't implement it which effectively does the same thing as this switch.
+add_compile_options(
+  -mcpu=cortex-m4
+  -march=armv7e-m
+  -mthumb
+  -mfloat-abi=hard
+  -mfpu=fpv4-sp-d16
+  -fno-use-cxa-atexit
+)
+
+include_directories(${tools}/arm-none-eabi/include)
+include_directories(${tools}/arm-none-eabi/include/newlib-nano)
+
+#libgcc.a is here
+link_directories(${tools}/lib/gcc/arm-none-eabi/9.2.1/thumb/v7e-m+fp/hard)
+#The rest of the libs are here.
+link_directories(${tools}/arm-none-eabi/lib/thumb/v7e-m+fp/hard)
+
+add_link_options(
+  -nostartfiles
+  -nodefaultlibs
+  -static
+  --specs=nano.specs
+  -Wl,-T${linkerScript}
+  -Wl,--gc-sections
+  -Wl,-Map,${CCS_PROJECT_LINK}.map
+  -Wl,--print-memory-usage
+)
+
+link_libraries(
+  gcc
+  c
+  m
+  nosys
+  stdc++
+)
